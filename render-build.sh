ffa8ad03cb4b229fcd2d6aaf193eb2479e5603dd
Saket Singh <93044592+SAKETSINGH120@users.noreply.github.com>
2025-08-02 16:20:50 +0530

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,28 +1,7 @@
 #!/usr/bin/env bash
 
-# Install necessary dependencies
-apt-get update
-apt-get install -y wget \
-    ca-certificates \
-    fonts-liberation \
-    libappindicator3-1 \
-    libasound2 \
-    libatk-bridge2.0-0 \
-    libatk1.0-0 \
-    libcups2 \
-    libdbus-1-3 \
-    libgdk-pixbuf2.0-0 \
-    libnspr4 \
-    libnss3 \
-    libx11-xcb1 \
-    libxcomposite1 \
-    libxdamage1 \
-    libxrandr2 \
-    xdg-utils \
-    --no-install-recommends
-
-# Install node modules
+# Step 1: Install node modules
 npm install
 
-# Download the Chromium browser Puppeteer needs
+# Step 2: Download the Chromium browser required by Puppeteer
 npx puppeteer browsers install chrome
